60dcb444790d65a8d5181a1ded0c05f65141f9ef
admin <kamil@cubesite.pl>
2024-05-14 11:12:22 +0200

    getPatientByLastName stack overflow

--- src/test/java/com/capgemini/wsb/persistence/dao/PatientDaoTest.java
+++ src/test/java/com/capgemini/wsb/persistence/dao/PatientDaoTest.java
@@ -1,7 +1,16 @@
 package com.capgemini.wsb.persistence.dao;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
+@Transactional
 class PatientDaoTest {
 
+    @Autowired
+    private PatientDao patientDao;
 }
